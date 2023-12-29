a885ba014cc5efdeaed3353a6c4eca2782f58a86
Roberto dos Santos Filho <robertodossantosfilho1001@gmail.com>
2023-12-29 17:15:10 -0400

    Controllers & Testes E2E - Outros controllers da academia

---
+++ src/http/controllers/gyms/nearby.ts
@@ -0,0 +1,23 @@
+import { makeFetchNearByGymsUseCase } from "@/use-Cases/factories/make-fetch-nearby-gyms-use-case";
+import { FastifyReply, FastifyRequest } from "fastify";
+import { z } from "zod";
+
+export async function nearby(request: FastifyRequest, reply: FastifyReply) {
+  const nearbyGymsQuerySchema = z.object({
+    latitude: z.number().refine((value) => {
+      return Math.abs(value) <= 90;
+    }),
+    longitude: z.number().refine((value) => {
+      return Math.abs(value) <= 180;
+    }),
+  });
+  const { latitude, longitude } = nearbyGymsQuerySchema.parse(request.body);
+  const fetchNearbyGymsUseCase = makeFetchNearByGymsUseCase();
+  const { gyms } = await fetchNearbyGymsUseCase.execute({
+    userLatitude: latitude,
+    userLongitude: longitude,
+  });
+  return reply.status(200).send({
+    gyms,
+  });
+}
