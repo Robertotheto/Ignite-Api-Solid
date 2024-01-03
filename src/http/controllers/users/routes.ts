d60b26601f34795132ba7abc7f7d76337dc009c5
Roberto dos Santos Filho <robertodossantosfilho1001@gmail.com>
2024-01-02 20:10:28 -0400

    Refresh Token & RBAC - Implementação do refresh token

--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -3,10 +3,12 @@ import { FastifyInstance } from "fastify";
 import { authenticate } from "../users/authenticate";
 import { profile } from "../users/profile";
 import { register } from "../users/register";
+import { refresh } from "./refresh";
 
 export async function usersRoutes(app: FastifyInstance) {
   app.post("/users", register);
   app.post("/sessions", authenticate);
+  app.patch("/token/refresh", refresh);
 
   // Authenticated
   app.get("/me", { onRequest: [verifyJwt] }, profile);
